5b11243db3d27a4f4759f8265aa63512999f7bb8
rleek <richard@richardleek.com>
2020-02-10 15:19:51 +0100

    load all item data

--- eoinfo/item.py
+++ eoinfo/item.py
@@ -1,6 +1,61 @@
+from enum import Enum
+
 class Item:
     def __init__(self):
         self.id = 0
 
     def print(self):
         print(f"{self.id} - {self.name}")
+
+class Special(Enum):
+    Normal = 0
+    Rare = 1
+    UnknownSpecial = 2
+    Unique = 3
+    Lore = 4
+    Cursed = 5
+
+class Type(Enum):
+    Static = 0
+    UnknownType1 = 1
+    Money = 2
+    Heal = 3
+    Teleport = 4
+    Spell = 5
+    EXPReward = 6
+    StatReward = 7
+    SkillReward = 8
+    Key = 9
+    Weapon = 10
+    Shield = 11
+    Armor = 12
+    Hat = 13
+    Boots = 14
+    Gloves = 15
+    Accessory = 16
+    Belt = 17
+    Necklace = 18
+    Ring = 19
+    Armlet = 20
+    Bracer = 21
+    Beer = 22
+    EffectPotion = 23
+    HairDye = 24
+    CureCurse = 25
+
+class SubType(Enum):
+    Nil = 0
+    Ranged = 1
+    Arrows = 2
+    Wings = 3
+    TwoHanded = 4
+
+class Size(Enum):
+    Size1x1 = 0
+    Size1x2 = 1
+    Size1x3 = 2
+    Size1x4 = 3
+    Size2x1 = 4
+    Size2x2 = 5
+    Size2x3 = 6
+    Size2x4 = 7
--- eoinfo/item_reader.py
+++ eoinfo/item_reader.py
@@ -1,4 +1,4 @@
-from item import Item
+from item import Item, Special, Type, SubType, Size
 import utils
 
 class ItemReader:
@@ -34,6 +34,59 @@ class ItemReader:
            return False
 
        buf = self.file.read(self.data_size)
+       item.graphic = utils.decode_number([buf[0], buf[1]])
+       item.type = Type(utils.decode_number([buf[2]]))
+       item.sub_type = SubType(utils.decode_number([buf[3]]))
+       item.special = Special(utils.decode_number([buf[4]])) 
+
+       item.hp = utils.decode_number([buf[5], buf[6]])
+       item.tp = utils.decode_number([buf[7], buf[8]])
+
+       item.min_dam = utils.decode_number([buf[9], buf[10]])
+       item.max_dam = utils.decode_number([buf[11], buf[12]])
+       item.accuracy = utils.decode_number([buf[13], buf[14]])
+       item.evade = utils.decode_number([buf[15], buf[16]])
+       item.armor = utils.decode_number([buf[17], buf[18]])
+
+       item.str = utils.decode_number([buf[20]])
+       item.int = utils.decode_number([buf[21]])
+       item.wis = utils.decode_number([buf[22]])
+       item.agi = utils.decode_number([buf[23]])
+       item.con = utils.decode_number([buf[24]])
+       item.cha = utils.decode_number([buf[25]])
+
+       item.light = utils.decode_number([buf[26]])
+       item.dark = utils.decode_number([buf[27]])
+       item.earth = utils.decode_number([buf[28]])
+       item.air = utils.decode_number([buf[29]])
+       item.water = utils.decode_number([buf[30]])
+       item.fire = utils.decode_number([buf[31]])
+
+       item.scroll_map = utils.decode_number([buf[32], buf[33], buf[34]])
+       item.doll_graphic = item.scroll_map
+       item.exp_reward = item.scroll_map
+       item.hair_color = item.scroll_map
+       item.effect = item.scroll_map
+       item.key = item.scroll_map
+
+       item.scroll_x = utils.decode_number([buf[35]])
+       item.gender = item.scroll_x
+
+       item.scroll_y = utils.decode_number([buf[36]])
+       item.dual_wield_doll_graphic = item.scroll_y
+
+       item.level_req = utils.decode_number([buf[37], buf[38]])
+       item.class_req = utils.decode_number([buf[39], buf[40]])
+
+       item.str_req = utils.decode_number([buf[41], buf[42]])
+       item.int_req = utils.decode_number([buf[43], buf[44]])
+       item.wis_req = utils.decode_number([buf[45], buf[46]])
+       item.agi_req = utils.decode_number([buf[47], buf[48]])
+       item.con_req = utils.decode_number([buf[49], buf[50]])
+       item.cha_req = utils.decode_number([buf[51], buf[52]])
+
+       item.weight = utils.decode_number([buf[55]])
+       item.size = Size(utils.decode_number([buf[57]]))
 
        self._item = item 
        self.position += 1
