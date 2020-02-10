4c5ba14a2bf4510d7dd0861daf676c2f1e93ca01
rleek <richard@richardleek.com>
2020-02-10 14:18:21 +0100

    added function for decoding eo numbers

---
+++ eoinfo/utils.py
@@ -0,0 +1,19 @@
+MAX1 = 253
+MAX2 = 64009
+MAX3 = 16194277
+
+def decode_number(bytes):
+    data = [1, 1, 1, 1]
+    for i in range(0, 4):
+        if len(bytes) > i:
+            data[i] = bytes[i]
+
+        if data[i] == 254:
+            data[i] = 1
+
+        if data[i] == 0:
+            data[i] = 128
+
+        data[i] -= 1
+
+    return data[3] * MAX3 + data[2] * MAX2 + data[1] * MAX1 + data[0]
